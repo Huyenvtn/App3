044fe211142c31a6935489a2347c244f18338746
Huyenvtn <huyenvtn0210@gmail.com>
2019-10-31 21:17:47 +0700

    db

--- App3/App3/Database.cs
+++ App3/App3/Database.cs
@@ -12,12 +12,76 @@ namespace App3
         {
             try
             {
-
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, "qlhoa.db")))
+                {
+                    connection.CreateTable<LoaiHoa>();
+                    connection.CreateTable<Hoa>();
+                    return true;
+                }
             }
-            catch()
+            catch(SQLiteException e)
             {
-
+                return false;
             }
+        }
+        public bool themLoaiHoa (LoaiHoa loai)
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, "qlhoa.db")))
+                {
+                    connection.Insert(loai);
+                    return true;
+                }
+            }
+            catch(SQLiteException e)
+            {
+                return false;
+            }
+        }
+        public List<LoaiHoa> chonLoaiHoa()
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, "qlhoa.db")))
+                {
+                    return connection.Table<LoaiHoa>().ToList();
+                }
+            }
+            catch (SQLiteException e)
+            {
+                return null;
+            }
+        }
+        public bool themHoa (Hoa hoa)
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, "qlhoa.db")))
+                {
+                    connection.Insert(hoa);
+                    return true;
+                }
+            }
+            catch(SQLiteException e)
+            {
+                return false;
+            }
+        }
+        public List<Hoa> chonHoa ()
+        {
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, "qlhoa.db")))
+                {
+                    return connection.Table<Hoa>().ToList();
+                }
+            }
+            catch (SQLiteException e)
+            {
+                return null;
+            }
+
         }
     }
 }
